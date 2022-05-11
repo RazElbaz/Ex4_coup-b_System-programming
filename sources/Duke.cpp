e61df2f3a0219ee6e5a4eeebc31eb6ad3e0eb70c
Raz Elbaz <93310416+RazElbaz@users.noreply.github.com>
2022-05-11 19:26:07 +0300

    Add files via upload

--- sources/Duke.cpp
+++ sources/Duke.cpp
@@ -4,29 +4,22 @@ int const minNumber=3;
 namespace coup{
     Duke::Duke(Game &Game, string Name) : Player(Game, Name){}
     void Duke::block(Player &player) {
+        //checking the game roles and the players roles
         if (!this->isAlive() || !player.isAlive()) { throw runtime_error("The players are not alive"); }
         if (!this->getGameName()->activeGame) { throw runtime_error("The game not active"); }
         if (player.YouCanLock && player.LastAction == "foreign aid") {
             player.YouCanLock = false;
             player.Coins -= 2;
         } else {
-            throw runtime_error("Cannot be Blocked!");
+            throw runtime_error("The Duke cannot block this player");
         }
 
     }
     void Duke::tax() {
-        if(!this->getGameName()->TheGameStart){
-            this->getGameName()->TheGameStart= true;
-        }
-        if(!this->getGameName()->activeGame){
-            throw runtime_error("The game not active");
-        }
-        if(!this->isAlive()){
-            throw runtime_error("This player is not alive");
-        }
-        if(this->getGameName()->turn()!=this->Name){
-            throw runtime_error("It's not Duke turn");
-        }
+        if(!this->getGameName()->TheGameStart){this->getGameName()->TheGameStart= true;}
+        if(!this->getGameName()->activeGame){throw runtime_error("The game not active");}
+        if(!this->isAlive()){throw runtime_error("This player is not alive");}
+        if(this->getGameName()->turn()!=this->Name){throw runtime_error("It's not Duke turn");}
         this->Coins+=minNumber;
         this->getGameName()->increaseTurn();
         this->YouCanLock= false;
